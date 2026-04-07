f0ff43e19c5331b087db9e59c214fab5d17d767c
codeinsight <120414369+shibaram-sathua@users.noreply.github.com>
2026-04-07 17:48:20 +0000

    add new count char

---
+++ src/String Problems/CountAChar.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class CountAChar {
+    static int countCharInAString(String str, char target){
+        for(int i = 0; i < str.length() - 1; i++){
+            
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string");
+        String str = sc.nextLine().trim();
+        char c = sc.next().trim().charAt(0);
+    }
+}
--- src/String Problems/Pallindrome.java
+++ src/String Problems/Pallindrome.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 public class Pallindrome {
     static String reverseStr(String str){
