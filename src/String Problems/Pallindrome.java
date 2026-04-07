58bcb31bd93abfa72c4f90ab816b78a2373d68cc
codeinsight <120414369+shibaram-sathua@users.noreply.github.com>
2026-04-07 13:46:50 +0000

    Add Pallindrome class to check for palindrome.

---
+++ src/String Problems/Pallindrome.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Pallindrome {
+    static String reverseStr(String str){
+        String reverse = "";
+        for(int i = str.length() - 1; i >= 0; i--){
+            reverse = reverse + str.charAt(i);
+        }
+        return reverse;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the sentence to check whether it is pallindrome or not!");
+        String str = sc.nextLine().trim().toLowerCase();
+       // System.out.println(reverseStr(str));
+        if(str.equals(reverseStr(str))){
+            System.out.println("pallondrome");
+        }
+        else{
+            System.out.println("Not pallindrome");
+        }
+        sc.close();
+    }
+}
