7dc928c1bf2193c1bfd2c477bb472d51457ad115
codeinsight <120414369+shibaram-sathua@users.noreply.github.com>
2026-04-07 13:22:13 +0000

    All files are add.

---
+++ src/String Problems/ReverseString.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ReverseString {
+    static String Solution1(String str){
+        String s1 = "";
+        for(int i = str.length() - 1; i >= 0; i--){
+            s1 = s1 + str.charAt(i);
+        }
+        return s1;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please provide the string:");
+        String s1 = new String();
+        s1 = sc.nextLine().trim();
+        System.out.println(ReverseString.Solution1(s1));
+    }
+}
